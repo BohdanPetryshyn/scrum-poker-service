5a5c9e2421b5a9d876c5e29197491847f2bd6d98
BohdanPetryshyn <b.y.petryshyn@gamil.com>
2020-04-30 09:45:59 +0300

    add resultCard to Voting response builder

--- utils/response/toVotingResponse.js
+++ utils/response/toVotingResponse.js
@@ -6,4 +6,5 @@ module.exports = voting => ({
   story: toStoryResponse(voting.story),
   estimates: voting.estimates && voting.estimates.map(toEstimateResponse),
   finishTime: voting.finishTime && voting.finishTime.getTime(),
+  resultCard: voting.resultCard,
 });
